5681de3f2ba9e6a392525b5ceb1aebcedd553b51
LBanos <lbanos@globalmaster.com.mx>
2022-08-19 10:54:20 -0500

    Modelos Producto y Venta agregados

---
+++ ventas_JoseAngel/Modelos/Productos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ventas_JoseAngel.Modelos
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        
+        public Productos() { }
+
+    }
+}
---
+++ ventas_JoseAngel/Modelos/Venta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ventas_JoseAngel.Modelos
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public int Id_Producto { get; set; }
+        public string Ticket { get; set; }
+        public decimal CantidadProducto { get; set; }
+        public decimal TotalProductos { get; set; } 
+
+        public Venta() { }
+    }
+}
